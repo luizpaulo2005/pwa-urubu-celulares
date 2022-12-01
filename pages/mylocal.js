29dd25cb062ed720bf54a5aecf61748367f81175
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2022-12-01 15:39:19 -0300

    prettier e head map

--- pages/mylocal.js
+++ pages/mylocal.js
@@ -1,15 +1,19 @@
-import Map from "../components/map"
-import NavBar from "../components/navbar"
+import Head from "next/head";
+import Map from "../components/map";
+import NavBar from "../components/navbar";
 
-const MeuLocal = () =>{
-    return(
-        <div className="container-fluid g-0">
-            <NavBar/>
-            <div className="d-flex flex-wrap justify-content-center container mt-2">
-                <Map/>
-            </div>
-        </div>
-    )
-}
+const MeuLocal = () => {
+  return (
+    <div className="container-fluid g-0">
+      <Head>
+        <title>Minha Localização</title>
+      </Head>
+      <NavBar />
+      <div className="d-flex flex-wrap justify-content-center container mt-2">
+        <Map />
+      </div>
+    </div>
+  );
+};
 
-export default MeuLocal
+export default MeuLocal;
